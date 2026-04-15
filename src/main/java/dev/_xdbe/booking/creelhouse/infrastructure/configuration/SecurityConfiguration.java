f88de119f5ca6677bc610cb472306e1607fa2ac2
Denoyelle <yohann.denoyelle@epita.fr>
2026-04-15 12:13:50 +0200

    feat: add login form

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
@@ -52,16 +52,16 @@ public class SecurityConfiguration {
         UserDetails administrator = User.builder()
             .username("admin")
             .password("{bcrypt}$2a$10$mszdPv220dAA78E643.Ege4lhqMUYv38cj4iwrKUJ8dQipNM2jHL2")
-            .roles("ADMIN") [cite: 194]
+            .roles("ADMIN")
             .build();
 
         UserDetails guest = User.builder()
             .username("guest")
             .password("{bcrypt}$2a$10$2cAXJutRgsKbMUCBTw0OfergYpkB2NAkGHlhWBpWJtbvkSmS3lcZu")
-            .roles("GUEST") [cite: 203]
+            .roles("GUEST")
             .build();
 
-        return new InMemoryUserDetailsManager(administrator, guest); [cite: 186]
+        return new InMemoryUserDetailsManager(administrator, guest);
     }
 
 }
